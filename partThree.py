e9e30782d38a7b2a1276113e4bc8e33ccc416d0e
JermXT <JermXT@users.noreply.github.com>
2017-09-17 11:58:15 -0700

    Updated comments

--- partThree.py
+++ partThree.py
@@ -1,25 +1,36 @@
+############################################################
 # Dp used
 # Runs in time O(N^2)
+############################################################
+
 file = open("coins2.in", "r")
 input = file.readline()
 file.close()
 input = input.rstrip()
 input = input.split(' ')
 coins = []
-solutionarray = []
 
 val = int(input[0])
 temp = val
+
 for x in range(1,5):
     coins.append(int(input[x]))
 coins.sort()
-count= 0
+
+############################################################
+# Dictionary to store values:solutions 
+############################################################
 d = {1:1}
 d[coins[1]] = 1
 d[coins[2]] = 1
 d[coins[3]] = 1
 
-min = 0
+############################################################
+# Finds solution of current value by iterating through
+# solutions of previous values
+############################################################
+
+
 for x in range(2, val+1):
     if x not in d:
         if x % 2 != 0:
@@ -29,16 +40,12 @@ for x in range(2, val+1):
         y = 1
         while y < len+1:
             temp = d[y]+d[x-y]
-            #print (x,y)
-            #print temp
             if(y == 1 or temp < min):
                 
                 min = temp
             y += 1
         d[x] = min
         
-#print d
-#print d[val]
 file = open("coins2.out", "w")
 
 file.write("%s" % (d[val]))

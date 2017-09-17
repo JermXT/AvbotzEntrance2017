01bb57fa6d7773525305bcc14fe7786896df5c51
Jeremy <Jeremy@macbook-pro.attlocal.net>
2017-09-17 16:52:43 -0700

    pid.py is file of constants/functions

--- partFive.py
+++ partFive.py
@@ -1,50 +1,48 @@
 import time
+import pid
 
-KP = 1.0
-KI = 1.0
-KD = 1.0
+#KP = 1.0
+#KI = 1.0
+#KD = 1.0
 
 print("Desired Height")
 endHeight = float(input())
 
-def getHeight():
-    #returns a float
-    return
-def setThrust(thrust):
-    return
+
 
 location = []
 location = list()
 #list of error values
 
 trapezoids = 0
+
 while(True):
-    currentHeight = getHeight()
+    currentHeight = pid.getHeight()
     error = endHeight - currentHeight
 
-    proportional = error * KP
+    proportional = error * pid.KP
 
-    if location[-1] <= 0.01:
+
+    if len(location) == 0 or location[-1] <= 0.01:
         #once ideal height obtained, stop expanding list
         location.append(error)
-    
-
-    derivative = (location[-1]*KD-location[-2]*KD)/0.5
+    if len(location)>1:
+        derivative = (location[-1]*pid.KD-location[-2]*pid.KD)/0.5
     #calculates derivative based off of height 0.5 seconds ago
     #Each error is multiplied by KD
 
     
     if len(location)<2:
-        setThrust(derivative+proportional)
+        pid.setThrust(proportional)
         #starting height is zero, integral = 0
     elif len(location)<3:
-        trapezoids = 0.25(2*(location[-1]*KI*2))
-        setThrust(derivative+integral+proportional)
-        trapezoids = location[-1]*KI
+        trapezoids = 0.25*(2*(location[-1]*pid.KI*2))
+        pid.setThrust(derivative+trapezoids+proportional)
+        trapezoids = location[-1]*pid.KI
         #handles case when there is one collumn
     else:
-        trapezoids += (location[-1]*KI + location[-2]*KI)
-        setThrust(0.25(trapezoids)+derivative+proportional)
+        trapezoids += (location[-1]*pid.KI + location[-2]*pid.KI)
+        pid.setThrust(0.25*(trapezoids)+derivative+proportional)
     #uses trapezoid rule to calculate integral
-    
+    print error
     time.sleep(0.5)
---
+++ pid.py
@@ -0,0 +1,9 @@
+KP = 1.0
+KI = 1.0
+KD = 1.0
+
+def getHeight():
+    height = 10
+    return height
+def setThrust(thrust):
+    return

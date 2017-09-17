fcb7ed9c5639cb2aec82f1e5b8d43dfad4b34674
JermXT <JermXT@users.noreply.github.com>
2017-09-17 11:14:55 -0700

    Bugfix

--- partTwo.py
+++ partTwo.py
@@ -1,8 +1,8 @@
 file = open("coins.in", "r")
 input = file.readline()
 file.close()
-val = float(a)
-val = a*100
+val = float(input)
+val = val*100
 val = int(val)
 coins = 0
 
@@ -11,15 +11,15 @@ while(val > 0):
         val -= 25
         coins += 1
         
-    if(val >= 10 and a < 25):
+    if(val >= 10 and val < 25):
         val -= 10
         coins += 1
         
-    if(val >= 5 and a < 10):
+    if(val >= 5 and val < 10):
         a -= 5
         coins += 1
         
-    if(val >= 1 and a < 5):
+    if(val >= 1 and val < 5):
         val -= 1
         coins += 1
         

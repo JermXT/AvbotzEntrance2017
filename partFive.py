51c89ca3a394649c51797e31978dcefea6438dac
Jeremy <Jeremy@macbook-pro.attlocal.net>
2017-09-17 22:35:22 -0700

    clarified integrals

--- partFive.py
+++ partFive.py
@@ -38,6 +38,7 @@ while(True):
     if len(location) == 0 or location[-1] <= 0.01:
         # once ideal height has been obtained, stop expanding list
         location.append(error)
+
     if len(location)>1:
         derivative = (location[-1]*pid.KD-location[-2]*pid.KD)/0.5
         # calculates derivative based off of height 0.5 seconds ago
@@ -48,13 +49,12 @@ while(True):
         pid.setThrust(proportional)
         # starting height is zero, integral = 0, no derivative
     elif len(location)<3:
-        trapezoids = 0.25*(2*(location[-1]*pid.KI*2))
+        trapezoids = (location[-1]*pid.KI)*0.5*0.5
         pid.setThrust(derivative+trapezoids+proportional)
-        trapezoids = location[-1]*pid.KI
         # handles case when there is one collumn
     else:
-        trapezoids += (location[-1]*pid.KI + location[-2]*pid.KI)
-        pid.setThrust(0.25*(trapezoids)+derivative+proportional)
+        trapezoids += 0.25*(location[-1]*pid.KI + location[-2]*pid.KI)
+        pid.setThrust(trapezoids+derivative+proportional)
         # uses trapezoid rule to calculate integral
     print error
     time.sleep(0.5)

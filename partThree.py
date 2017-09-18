7d1c0013e7020f42098f8a71bb4668fa9593429f
Jeremy <Jeremy@macbook-pro.attlocal.net>
2017-09-17 21:48:00 -0700

    more comments formatting

--- partThree.py
+++ partThree.py
@@ -17,23 +17,25 @@ for x in range(1,5):
     coins.append(int(input[x]))
 coins.sort()
 
-############################################################
+#-----------------------------------------------------------
 # Dictionary to store values:solutions 
-############################################################
+#-----------------------------------------------------------
 d = {1:1}
 d[coins[1]] = 1
 d[coins[2]] = 1
 d[coins[3]] = 1
 
-############################################################
+#-----------------------------------------------------------
 # Finds solution of current value by iterating through
 # solutions of previous values
-############################################################
+#-----------------------------------------------------------
 
 
 for x in range(2, val+1):
     min = 100000000000000000
     if x not in d:
+        # finds solution of value minus 
+        # each coin, checks which is smallest.
         if x > coins[3]:
             if min > d[x-coins[3]]+1:
                 min = d[x-coins[3]]+1
@@ -48,7 +50,3 @@ for x in range(2, val+1):
                 min = d[x-coins[0]]+1
         d[x] = min
 print d[val]
-############################################################
-# finds solution of value minus each coin, checks which is
-# smallest.
-############################################################

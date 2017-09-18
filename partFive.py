71bb4527f87402c2ac0fed2653db909ac630ba0a
Jeremy <Jeremy@macbook-pro.attlocal.net>
2017-09-17 22:03:39 -0700

    Updated comments

--- partFive.py
+++ partFive.py
@@ -1,14 +1,8 @@
 import time
 import pid
 
-#KP = 1.0
-#KI = 1.0
-#KD = 1.0
-
 print("Desired Height")
 endHeight = float(input())
-
-
 
 location = []
 location = list()
@@ -16,6 +10,11 @@ location = list()
 
 trapezoids = 0
 
+#----------------------------------------------------------- 
+# Calculates Proportional, Integral, and Derivative every
+# 0.5 seconds. Adds these together to detirmine the thrust
+# of the quadcopter.
+#----------------------------------------------------------- 
 while(True):
     currentHeight = pid.getHeight()
     error = endHeight - currentHeight
@@ -24,25 +23,25 @@ while(True):
 
 
     if len(location) == 0 or location[-1] <= 0.01:
-        #once ideal height obtained, stop expanding list
+        # once ideal height has been obtained, stop expanding list
         location.append(error)
     if len(location)>1:
         derivative = (location[-1]*pid.KD-location[-2]*pid.KD)/0.5
-    #calculates derivative based off of height 0.5 seconds ago
-    #Each error is multiplied by KD
+        # calculates derivative based off of height 0.5 seconds ago
+        # Each error is multiplied by KD
 
     
     if len(location)<2:
         pid.setThrust(proportional)
-        #starting height is zero, integral = 0
+        # starting height is zero, integral = 0, no derivative
     elif len(location)<3:
         trapezoids = 0.25*(2*(location[-1]*pid.KI*2))
         pid.setThrust(derivative+trapezoids+proportional)
         trapezoids = location[-1]*pid.KI
-        #handles case when there is one collumn
+        # handles case when there is one collumn
     else:
         trapezoids += (location[-1]*pid.KI + location[-2]*pid.KI)
         pid.setThrust(0.25*(trapezoids)+derivative+proportional)
-    #uses trapezoid rule to calculate integral
+        # uses trapezoid rule to calculate integral
     print error
     time.sleep(0.5)
